079872e2796008c48d7ca11b6b21da6c4c1010d7
Mayya Markova <mayya.markova@gmail.com>
2023-03-24 16:32:22 +0200

    Set up postgres db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
@@ -11,6 +15,17 @@ import { UsersModule } from './users/users.module';
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: './schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UsersModule,
   ],
