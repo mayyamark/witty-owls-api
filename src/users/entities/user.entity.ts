ed39507276cba0e11a8f604ebbddfa23902440bb
Mayya Markova <mayya.markova@gmail.com>
2023-03-24 16:37:20 +0200

    Update the structure of User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,15 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'ID of the user' })
+  id: string;
+  @Field(() => String, { description: 'First name of the user' })
+  firstName: string;
+  @Field(() => String, { description: 'Last name of the user' })
+  lastName: string;
+  @Field(() => String, { description: 'Email of the user' })
+  email: string;
+  @Field(() => String, { description: 'Role of the user' })
+  role: string;
 }
